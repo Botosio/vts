7c82eb944997ef09b25e5705c8fdbde146e213ea
Developer.Dave 🧙‍♂️ <44526601+Botosio@users.noreply.github.com>
2020-08-07 08:24:07 -0400

    custom theme and pre-dashboard added

--- apps/vts/src/app/app-routing.module.ts
+++ apps/vts/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'camera',
+    // canActivate: [CanActivateRouteGuard],
+    loadChildren: () => import('@vts/camera-lib').then((m) => m.CameraModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- libs/camera/src/lib/camera.module.ts
+++ libs/camera/src/lib/camera.module.ts
@@ -4,10 +4,23 @@ import { RouterModule, Route } from '@angular/router';
 import { CameraListComponent } from './camera-list/camera-list.component';
 import { CameraDetailComponent } from './camera-detail/camera-detail.component';
 
-export const cameraRoutes: Route[] = [];
+export const cameraRoutes: Route[] = [
+  {
+    path: '',
+    component: CameraListComponent,
+  },
+  {
+    path: 'new',
+    component: CameraDetailComponent,
+  },
+  {
+    path: ':id',
+    component: CameraDetailComponent,
+  },
+];
 
 @NgModule({
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule.forChild(cameraRoutes)],
   declarations: [CameraListComponent, CameraDetailComponent],
   exports: [CameraListComponent, CameraDetailComponent],
 })
